a07a13a0024f0b8bddfe69994e2c210f3b7588f7
Rodolfo Viti <rodolfo.viti@nttdata.com>
2022-05-02 16:06:53 +0200

    fix unit tests

--- web/src/test/java/it/pagopa/pdv/tokenizer/web/controller/TokenizerControllerTest.java
+++ web/src/test/java/it/pagopa/pdv/tokenizer/web/controller/TokenizerControllerTest.java
@@ -1,29 +1,28 @@
 package it.pagopa.pdv.tokenizer.web.controller;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import it.pagopa.pdv.tokenizer.TestUtils;
+import com.jayway.jsonpath.JsonPath;
 import it.pagopa.pdv.tokenizer.connector.model.TokenDto;
 import it.pagopa.pdv.tokenizer.core.TokenizerService;
 import it.pagopa.pdv.tokenizer.web.config.WebTestConfig;
 import it.pagopa.pdv.tokenizer.web.handler.RestExceptionsHandler;
-import it.pagopa.pdv.tokenizer.web.model.PiiResource;
-import it.pagopa.pdv.tokenizer.web.model.TokenResource;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.core.io.Resource;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.util.UUID;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 @WebMvcTest(value = {TokenizerController.class}, excludeAutoConfiguration = SecurityAutoConfiguration.class)
 @ContextConfiguration(classes = {
@@ -42,61 +41,56 @@ class TokenizerControllerTest {
     @Autowired
     protected MockMvc mvc;
 
-    @Autowired
-    protected ObjectMapper objectMapper;
-
 
     @Test
-    void save() throws Exception {
+    void save(@Value("classpath:stubs/piiResource.json") Resource piiResource) throws Exception {
         // given
         String namespace = "selfcare";
-        PiiResource piiResource = TestUtils.mockInstance(new PiiResource());
         TokenDto tokenDto = new TokenDto();
         tokenDto.setToken(UUID.randomUUID().toString());
         tokenDto.setRootToken(UUID.randomUUID().toString());
         Mockito.when(tokenizerServiceMock.save(Mockito.anyString(), Mockito.anyString()))
                 .thenReturn(tokenDto);
         // when
-        MvcResult result = mvc.perform(MockMvcRequestBuilders
+        mvc.perform(MockMvcRequestBuilders
                 .put(BASE_URL + "/")
                 .header(NAMESPACE_HEADER_NAME, namespace)
-                .content(objectMapper.writeValueAsString(piiResource))
+                .content(piiResource.getInputStream().readAllBytes())
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
                 .accept(MediaType.APPLICATION_JSON_VALUE))
                 .andExpect(MockMvcResultMatchers.status().isOk())
-                .andReturn();
+                .andExpect(jsonPath("$.token", notNullValue()))
+                .andExpect(jsonPath("$.rootToken", notNullValue()));
         // then
-        TokenResource tokenResource = objectMapper.readValue(result.getResponse().getContentAsString(), TokenResource.class);
-        assertNotNull(tokenResource);
+        String stubbedPii = JsonPath.parse(piiResource.getInputStream()).read("$.pii", String.class);
         Mockito.verify(tokenizerServiceMock, Mockito.times(1))
-                .save(piiResource.getPii(), namespace);
+                .save(stubbedPii, namespace);
         Mockito.verifyNoMoreInteractions(tokenizerServiceMock);
     }
 
 
     @Test
-    void search() throws Exception {
+    void search(@Value("classpath:stubs/piiResource.json") Resource piiResource) throws Exception {
         String namespace = "selfcare";
-        PiiResource piiResource = TestUtils.mockInstance(new PiiResource());
         TokenDto tokenDto = new TokenDto();
         tokenDto.setToken(UUID.randomUUID().toString());
         tokenDto.setRootToken(UUID.randomUUID().toString());
         Mockito.when(tokenizerServiceMock.findById(Mockito.anyString(), Mockito.anyString()))
                 .thenReturn(tokenDto);
         // when
-        MvcResult result = mvc.perform(MockMvcRequestBuilders
+        mvc.perform(MockMvcRequestBuilders
                 .post(BASE_URL + "/search")
                 .header(NAMESPACE_HEADER_NAME, namespace)
-                .content(objectMapper.writeValueAsString(piiResource))
+                .content(piiResource.getInputStream().readAllBytes())
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
                 .accept(MediaType.APPLICATION_JSON_VALUE))
                 .andExpect(MockMvcResultMatchers.status().isOk())
-                .andReturn();
+                .andExpect(jsonPath("$.token", notNullValue()))
+                .andExpect(jsonPath("$.rootToken", notNullValue()));
         // then
-        TokenResource tokenResource = objectMapper.readValue(result.getResponse().getContentAsString(), TokenResource.class);
-        assertNotNull(tokenResource);
+        String stubbedPii = JsonPath.parse(piiResource.getInputStream()).read("$.pii", String.class);
         Mockito.verify(tokenizerServiceMock, Mockito.times(1))
-                .findById(piiResource.getPii(), namespace);
+                .findById(stubbedPii, namespace);
         Mockito.verifyNoMoreInteractions(tokenizerServiceMock);
     }
 
@@ -109,15 +103,13 @@ class TokenizerControllerTest {
         Mockito.when(tokenizerServiceMock.findPiiByToken(Mockito.anyString()))
                 .thenReturn(pii);
         // when
-        MvcResult result = mvc.perform(MockMvcRequestBuilders
+        mvc.perform(MockMvcRequestBuilders
                 .get(BASE_URL + "/{token}/pii", token)
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
                 .accept(MediaType.APPLICATION_JSON_VALUE))
                 .andExpect(MockMvcResultMatchers.status().isOk())
-                .andReturn();
+                .andExpect(jsonPath("$.pii", notNullValue()));
         // then
-        PiiResource piiResource = objectMapper.readValue(result.getResponse().getContentAsString(), PiiResource.class);
-        assertNotNull(piiResource);
         Mockito.verify(tokenizerServiceMock, Mockito.times(1))
                 .findPiiByToken(token.toString());
         Mockito.verifyNoMoreInteractions(tokenizerServiceMock);
