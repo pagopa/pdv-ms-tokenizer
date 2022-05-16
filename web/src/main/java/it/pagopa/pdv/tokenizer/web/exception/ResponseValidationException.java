ac5f13d62861643dca986d1d967119c75c6b2779
Rodolfo Viti <62432865+rodoviti@users.noreply.github.com>
2022-05-16 19:07:00 +0200

    sonarcloud code smells fixes (#5)
    * sonarcloud code smells fixes
    
    * sonarcloud code smells fixes
    
    Co-authored-by: Rodolfo Viti <rodolfo.viti@nttdata.com>

---
+++ web/src/main/java/it/pagopa/pdv/tokenizer/web/exception/ResponseValidationException.java
@@ -0,0 +1,9 @@
+package it.pagopa.pdv.tokenizer.web.exception;
+
+public class ResponseValidationException extends RuntimeException {
+
+    public ResponseValidationException(String message) {
+        super(message);
+    }
+
+}
---
+++ web/src/test/java/it/pagopa/pdv/tokenizer/web/controller/DummyModel.java
@@ -0,0 +1,15 @@
+package it.pagopa.pdv.tokenizer.web.controller;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotNull;
+
+@NoArgsConstructor
+@AllArgsConstructor
+public class DummyModel {
+
+    @NotNull
+    String value;
+
+}
