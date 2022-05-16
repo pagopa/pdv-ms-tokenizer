0c10c413198086d2adcf5f6c22be43880ea999ed
Rodolfo Viti <rodolfo.viti@nttdata.com>
2022-05-16 18:53:31 +0200

    sonarcloud code smells fixes

--- web/src/test/java/it/pagopa/pdv/tokenizer/web/validator/TokenizerControllerResponseValidatorTest.java
+++ web/src/test/java/it/pagopa/pdv/tokenizer/web/validator/TokenizerControllerResponseValidatorTest.java
@@ -1,6 +1,7 @@
 package it.pagopa.pdv.tokenizer.web.validator;
 
 import it.pagopa.pdv.tokenizer.web.controller.DummyController;
+import it.pagopa.pdv.tokenizer.web.exception.ResponseValidationException;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,7 @@ import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
@@ -30,7 +32,16 @@ class TokenizerControllerResponseValidatorTest {
 
     @Test
     void controllersPointcut_returnNotVoid() {
-        controller.notVoidMethod();
+        assertDoesNotThrow(() -> controller.notVoidMethodValidResult());
+        verify(validatorSpy, Mockito.times(1))
+                .validateResponse(any(), any());
+        verifyNoMoreInteractions(validatorSpy);
+    }
+
+
+    @Test
+    void controllersPointcut_returnNotVoidButInvalid() {
+        assertThrows(ResponseValidationException.class, () -> controller.notVoidMethodInvalidResult());
         verify(validatorSpy, Mockito.times(1))
                 .validateResponse(any(), any());
         verifyNoMoreInteractions(validatorSpy);
@@ -39,7 +50,7 @@ class TokenizerControllerResponseValidatorTest {
 
     @Test
     void controllersPointcut_returnVoid() {
-        controller.voidMethod();
+        assertDoesNotThrow(() -> controller.voidMethod());
         verify(validatorSpy, Mockito.times(1))
                 .validateResponse(any(), any());
         verifyNoMoreInteractions(validatorSpy);
