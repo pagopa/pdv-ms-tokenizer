cc399f10aa693e4ca5c717b18ac82dc5bf94f13e
Rodolfo Viti <rodolfo.viti@nttdata.com>
2022-06-22 13:21:02 +0200

    bugfixing
    - fix log
    - fix (enable) validation on API model
    - improve error response model

---
+++ web/src/main/java/it/pagopa/pdv/tokenizer/web/model/mapper/ProblemMapper.java
@@ -0,0 +1,17 @@
+package it.pagopa.pdv.tokenizer.web.model.mapper;
+
+import it.pagopa.pdv.tokenizer.web.model.Problem;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+
+import static org.springframework.http.MediaType.APPLICATION_PROBLEM_JSON;
+
+public class ProblemMapper {
+
+    public static ResponseEntity<Problem> toResponseEntity(Problem problem) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(APPLICATION_PROBLEM_JSON);
+        return new ResponseEntity<>(problem, headers, problem.getStatus());
+    }
+
+}
