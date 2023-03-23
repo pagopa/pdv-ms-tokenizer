7f6102d33a81d7e58f998cb878fed34680553ee4
Rodolfo Viti <rodolfo.viti@pagopa.it>
2023-03-23 17:41:25 +0100

    fix generateCreateTableRequest

--- connector/dao/src/test/java/it/pagopa/pdv/tokenizer/connector/dao/config/DaoTestConfig.java
+++ connector/dao/src/test/java/it/pagopa/pdv/tokenizer/connector/dao/config/DaoTestConfig.java
@@ -6,6 +6,11 @@ import com.amazonaws.services.dynamodbv2.model.CreateTableRequest;
 import com.amazonaws.services.dynamodbv2.model.ListTablesResult;
 import com.amazonaws.services.dynamodbv2.model.ProjectionType;
 import com.amazonaws.services.dynamodbv2.model.ProvisionedThroughput;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import it.pagopa.pdv.tokenizer.connector.dao.TokenizerConnectorImpl;
 import it.pagopa.pdv.tokenizer.connector.dao.model.NamespacedFiscalCodeToken;
 import org.junit.jupiter.api.Test;
@@ -48,7 +53,15 @@ public class DaoTestConfig {
         CreateTableRequest tableRequest = createCreateTableRequest(dynamoDBMapper);
         Path basePath = Paths.get("target/test/dynamodb-local-table-template/");
         Files.createDirectories(basePath);
-        Files.write(basePath.resolve("Token.json"), tableRequest.toString().getBytes());
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.setPropertyNamingStrategy(PropertyNamingStrategy.UPPER_CAMEL_CASE);
+        objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+        String formatted = objectMapper.writeValueAsString(tableRequest);
+        JsonNode jsonNode = objectMapper.readTree(formatted);
+        ((ObjectNode) jsonNode).remove("RequestClientOptions");
+        ((ObjectNode) jsonNode).remove("GeneralProgressListener");
+        ((ObjectNode) jsonNode).remove("ReadLimit");
+        Files.write(basePath.resolve("Token.json"), jsonNode.toPrettyString().getBytes());
     }
 
 }
