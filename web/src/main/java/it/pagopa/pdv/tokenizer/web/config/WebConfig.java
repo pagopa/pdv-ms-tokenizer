cec97b088182cdba4cacf0ada27bc0b22b1cd7fc
Benito Visone <benito.visone@pagopa.it>
2023-12-13 15:27:48 +0100

    feat: Add XRayRecorderBuilder configuration (#100)

--- web/src/main/java/it/pagopa/pdv/tokenizer/web/config/WebConfig.java
+++ web/src/main/java/it/pagopa/pdv/tokenizer/web/config/WebConfig.java
@@ -1,6 +1,12 @@
 package it.pagopa.pdv.tokenizer.web.config;
 
+import com.amazonaws.xray.AWSXRay;
+import com.amazonaws.xray.AWSXRayRecorderBuilder;
 import com.amazonaws.xray.jakarta.servlet.AWSXRayServletFilter;
+import com.amazonaws.xray.plugins.EC2Plugin;
+import com.amazonaws.xray.plugins.ECSPlugin;
+import com.amazonaws.xray.strategy.jakarta.SegmentNamingStrategy;
+import com.amazonaws.xray.strategy.sampling.CentralizedSamplingStrategy;
 import jakarta.servlet.Filter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ApplicationContext;
@@ -43,8 +49,13 @@ class WebConfig implements WebMvcConfigurer {
         configurer.setUseTrailingSlashMatch(true);
     }
 
+    static {
+        AWSXRayRecorderBuilder builder = AWSXRayRecorderBuilder.standard().withPlugin(new ECSPlugin()).withPlugin(new EC2Plugin());
+        builder.withSamplingStrategy(new CentralizedSamplingStrategy());
+        AWSXRay.setGlobalRecorder(builder.build());
+    }
     @Bean
     public Filter TracingFilter() {
-        return new AWSXRayServletFilter("tokenizer-d-service-tokenizer");
+        return new AWSXRayServletFilter(SegmentNamingStrategy.dynamic(this.applicationContext.getId()));
     }
 }
